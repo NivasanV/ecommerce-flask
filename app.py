fcffa06fc42b9f8b8cd6e9324466f30c0156cd44
NivasanV <nivasanpersonalid@gmail.com>
2023-10-08 11:46:43 +0530

    account-page

--- app.py
+++ app.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
+@app.route('/products')
+def products():
+   return render_template('products.html')
+
+@app.route('/product-1-details')
+def product_1_details():
+   return render_template('product-1-details.html')
+
+@app.route('/account')
+def account():
+   return render_template('account.html')
+
+@app.route('/cart')
+def cart():
+   return render_template('cart.html')
+
 if __name__ == '__main__':
    app.run(debug = True)
