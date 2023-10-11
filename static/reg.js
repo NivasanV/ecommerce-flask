fe6f8d2d8f90bfa92353a4b577d223e82f954378
Nivasan <58383171+NivasanV@users.noreply.github.com>
2023-10-11 10:18:47 +0000

    register-firebase-done

--- static/reg.js
+++ static/reg.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,7 +26,7 @@ var email = document.getElementById("reg_email");
 var password = document.getElementById("reg_password");
 
 //making an function for storing data
-window.signup = function(e){
+window.register = function(e){
     e.preventDefault();
     var obj = {
         user_name : user_name.ariaValueMax,
